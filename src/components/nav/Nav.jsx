dc736d754982573b2ef4d52accb1f10f9623e13a
DavidTorresOvallos <davidtorresovallos@gmail.com>
2023-01-10 07:28:37 -0500

    Header finalizado y comienzo del nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BsJournalBookmark} from 'react-icons/bs'
+import {MdOutlineDesignServices} from 'react-icons/md'
+import {MdOutlinePermContactCalendar} from 'react-icons/md'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><BsJournalBookmark/></a>
+      <a href="#services"><MdOutlineDesignServices/></a>
+      <a href="#"><MdOutlinePermContactCalendar/></a> 
+    </nav>
   )
 }
 
