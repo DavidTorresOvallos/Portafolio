10f45c9b3ffedeca8dc17fcbea6b537732a7592f
DavidTorresOvallos <davidtorresovallos@gmail.com>
2023-01-17 07:09:25 -0500

    nav states

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,14 +5,17 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BsJournalBookmark} from 'react-icons/bs'
 import {MdOutlineDesignServices} from 'react-icons/md'
 import {MdOutlinePermContactCalendar} from 'react-icons/md'
+import { useState } from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><AiOutlineHome/></a>
-      <a href="#about"><AiOutlineUser/></a>
-      <a href="#experience"><BsJournalBookmark/></a>
-      <a href="#services"><MdOutlineDesignServices/></a>
-      <a href="#"><MdOutlinePermContactCalendar/></a> 
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsJournalBookmark/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdOutlineDesignServices/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlinePermContactCalendar/></a> 
     </nav>
   )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,7 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portofolio'>Portfolio</section>
   )
 }
 
