26a3d157e0d7a747628d934d1b50d5ed12433c7e
DavidTorresOvallos <davidtorresovallos@gmail.com>
2023-02-08 15:05:09 -0500

    Iniciando Experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './experience.css'
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>The Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience___containe">
+        <div className="experience__frontend"></div>
+      </div>
+    </section>
   )
 }
 
